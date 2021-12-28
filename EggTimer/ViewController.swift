ed3a2c17bc15a0b1356d00b50c946e3e95df2c3f
Bashir Yesufu <yesufubashir@gmail.com>
2021-12-29 00:13:41 +0100

    Play alert sound once egg is done

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
     var timer: Timer = Timer()
+    var player: AVAudioPlayer!
     var secondsPassed: Int = 0
     var totalTime: Int = 0
     let eggTimes: [String: Int] = [
@@ -45,6 +47,10 @@ class ViewController: UIViewController {
         } else {
             timer.invalidate()
             titleLabel.text = "DONE!"
+            
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
         }
     }
 }
